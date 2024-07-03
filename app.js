821732b46f5d1dce1df74802f99a9e3037f281d9
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-03 19:19:18 +0800

    Added functions that shows current scores and show winner

--- app.js
+++ app.js
@@ -39,67 +39,94 @@ buttons.forEach(button => {
     
 })
 
+function showResult(text){
+    const div = document.querySelector(".results");
+    const result = document.createElement("p");
+    result.setAttribute("class", "results");
+    result.textContent = text;
+    div.appendChild(result);
+}
+
+function showScores(text1, text2){
+    const div = document.querySelector(".results");
+    const hScore = document.createElement("p");
+    const cScore = document.createElement("p");
+
+    hScore.setAttribute("class", "results");
+    hScore.textContent = `${text1} ${humanScore}`;
+    div.appendChild(hScore);
+
+    cScore.classList.add("results");
+    cScore.textContent = `${text2} ${computerScore}`;
+    div.appendChild(cScore);
+}
+
+function showWinner(string){
+    const div = document.querySelector(".results");
+    const para = document.createElement("p");
+    para.classList.add("results");
+    para.textContent = string;
+    div.appendChild(para);
+}
+
+function checkScore(computerScore, humanScore){
+    if (computerScore > humanScore && computerScore === 5) {
+        showWinner("You Lose!")
+    }else if (computerScore < humanScore && humanScore === 5) {
+        showWinner("You Win!");
+    }
+}
 
 function playRound(computerChoice, humanChoice){
     if(computerChoice === "rock"){
         if(humanChoice === "paper"){
             humanScore++;
-            console.log("You win! Paper beats rock!");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("You win! Paper beats rock!");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }else if(humanChoice === "scissor"){
             computerScore++;
-            console.log("You Lose! Rock beats scissor!");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("You Lose! Rock beats scissor!");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }else{
-            console.log("It's a tie, try again");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("It's a tie, try again");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }
     }
     else if(computerChoice === "paper"){
         if(humanChoice === "paper"){
-            console.log("It's a tie, try again");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("It's a tie, try again.");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }else if(humanChoice === "scissor"){
             humanScore++;
-            console.log("You Win! Scissor beats paper");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("Youw Win! Scissor beats paper");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }else{
             computerScore++;
-            console.log("You lose! Paper beats rock");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("You lose! Paper beats rock");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }
     }else{
         if(humanChoice === "paper"){
             computerScore++
-            console.log("You lose! Scissor beats paper");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("You lose! Scissor beats paper");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }else if(humanChoice === "scissor"){
-            console.log("It's a tie! try again");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("It's a tie! try again");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }else{
             humanScore++
-            console.log("You win! Rock beats scissor");
-            console.log("Your score: ", humanScore);
-            console.log("Computer core: ", computerScore);
+            showResult("You win! Rock beats scissor");
+            showScores("Human score is: ", "Computer score is: ");
+            checkScore(computerScore, humanScore);
         }
     }
 }
 
-
-
-
-// if (computerScore > humanScore) {
-//     console.log("Computer wins!")
-// }else if (computerScore < humanScore ) {
-//     console.log("You win!!")
-// }else{
-//     console.log("It's a tie");
-// }
